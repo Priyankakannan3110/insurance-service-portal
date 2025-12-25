beb4b45b32e01f0bcf87995bd9d97644e3d2f9d8
Priyanka <priyankakannan3110@gmail.com>
2025-12-25 16:15:02 +0530

    Initial project setup

---
+++ src/test/java/com/insuranceservice/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insuranceservice.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
