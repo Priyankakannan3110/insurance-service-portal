190eaa17df7c43c68f4e433c20d4eb815f432ace
Priyanka <priyankakannan3110@gmail.com>
2025-12-27 17:31:35 +0530

    Basic CRUD working

---
+++ src/main/java/com/insuranceservice/backend/entity/User.java
@@ -0,0 +1,22 @@
+package com.insuranceservice.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+}
---
+++ src/main/java/com/insuranceservice/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.insuranceservice.backend.repository;
+
+import com.insuranceservice.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
